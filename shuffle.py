bd594f2221a687a52f429bcfd009c351ac744372
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2023-02-27 03:41:07 -0500

    Update shuffle.py
    Added comments for clarity

--- shuffle.py
+++ shuffle.py
@@ -3,11 +3,20 @@
 import random
 from PyPDF4 import PdfFileReader, PdfFileWriter
 
+# file name = original.pdf
 with open('original.pdf', 'rb') as pdf_file:
+    
+    # Open File
     pdf_reader = PdfFileReader(pdf_file)
+    
+    # Create a list of page numbers
     num_pages = pdf_reader.getNumPages()
     page_numbers = list(range(num_pages))
+    
+    # Shuffle the page numbers
     random.shuffle(page_numbers)
+    
+    # Create a new pdf with the shuffled pages
     pdf_writer = PdfFileWriter()
     for page_number in page_numbers:
         pdf_writer.addPage(pdf_reader.getPage(page_number))
